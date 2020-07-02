06f9607263e541a0e2ba4184cf161f11d0c77f2a
hungnn <hungnn@saltlux.com>
2020-07-02 15:02:51 +0700

    commit

---
+++ src/main/webapp/app/ngx-admin/pages/extra-components/infinite-list/news-post/news-post.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+import { NewsPost } from '../../services/news.service';
+
+@Component({
+    selector: 'ngx-news-post',
+    templateUrl: 'news-post.component.html'
+})
+export class NewsPostComponent {
+    @Input() post: NewsPost;
+}
